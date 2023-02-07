806603a4af78dc0fcc2de5edfc4ffb246a2a7120
JeongWooKim <mvp0729@gmail.com>
2023-02-07 19:36:49 +0900

    work die hard

--- baekjoon_step/16.백트래킹/9663_NQueen.py
+++ baekjoon_step/16.백트래킹/9663_NQueen.py
@@ -1,7 +1,40 @@
-def change(cmap,pos):
+import copy
+def rec(N,Map,Total_len):
+    global cnt
+    global result
+    # reccurent
+    if N==Total_len:
+        Q_cnt=0
+        for iqqi in range(Total_len):
+            Q_cnt+=Map[iqqi].count(5)
+        if Q_cnt==Total_len:
+            if Map not in result:
+                for ab in range(Total_len):
+                    print(Map[ab])
+                print()
+                cnt+=1
+                result.append(Map)
+        return
+    
+    CanGo=[]
+    for idx in range(Total_len):
+        if Map[N][idx]==1:
+                CanGo.append(idx)
+    if CanGo==[]:
+        rec(N+1,Map,Total_len)
+    else :
+        for i in CanGo:
+            local_map=copy.deepcopy(Map)
+            NewMap=change(Map,[N,i])
+            rec(N+1,NewMap,Total_len)
+            Map=copy.deepcopy(local_map)
+
+
+
+def change(inmap,pos):
+    cmap=inmap
     leng=len(cmap)
     raN=range(leng)
-
     cmap[pos[0]][:]=[0 for _ in raN] 
     # 열을 0으로
     for tmp in range(leng):
@@ -14,41 +47,29 @@ def change(cmap,pos):
             if pos[1]-k>=0 and pos[1]-k<leng:
                 cmap[pos[0]+k][pos[1]-k]=0
     cmap[pos[0]][pos[1]]=5
-    # print('=====================')
-    # for prl in raN:
-    #     print(cmap[prl])
-    # print('=====================')
     return cmap
 
 def main():
     N=int(input())
-
     raN=range(N)
-    set_end_chess_map=[]
     global cnt
+    global result
+    result=[]
     cnt=0
-
     for i in raN:
         for j in raN:
             chess_map=[[1 for _ in raN] for _ in raN]
             chess_map=change(chess_map,[i,j])
-            for k in raN:
-                for l in raN:
-                    if chess_map[k][l]==1:
-                        chess_map=change(chess_map,[k,l])
+            rec(0,chess_map,N)
 
-            cnt_q=0
-            for iqqi in raN:
-                if 5 in chess_map[iqqi]:
-                    cnt_q+=1
-            if cnt_q==N:
-                cnt+=1
-                for prl in raN:
-                    print(chess_map[prl])
-                print()
-        
-    print(len(set_end_chess_map))
+
     print('cnt=',cnt)
 
 if __name__=="__main__":
     main()
+
+
+"""
+answer = [0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596]
+print(answer[int(input())])
+"""
---
+++ baekjoon_step/16.백트래킹/9663_NQueen_a.py
@@ -0,0 +1,75 @@
+import copy
+def rec(floor,Map):
+    # reccurent
+    if floor==N:
+        Q_cnt=0
+        for iqqi in range(N):
+            Q_cnt+=Map[iqqi].count(5)
+        if Q_cnt==N:
+                result.append(Map)
+        return
+    CanGo=[]
+    for idx in range(N):
+        if Map[floor][idx]==1:
+                CanGo.append(idx)
+    if CanGo==[]:
+        rec(floor+1,Map)
+    else :
+        for i in CanGo:
+            local_map=copy.deepcopy(Map)
+            NewMap=change(Map,[floor,i])
+            rec(floor+1,NewMap)
+            Map=local_map
+
+def change(cmap,pos):
+    leng=len(cmap)
+    raN=range(leng)
+    cmap[pos[0]][:]=[0 for _ in raN] 
+    # 열을 0으로
+    for tmp in range(leng):
+        cmap[tmp][pos[1]]=0
+    for k in range(-leng+1,leng): # 수정필요할지도..
+        if pos[0]+k>=0 and pos[0]+k<leng:
+            if pos[1]+k>=0 and pos[1]+k<leng:
+                cmap[pos[0]+k][pos[1]+k]=0
+        if pos[0]+k>=0 and pos[0]+k<leng:
+            if pos[1]-k>=0 and pos[1]-k<leng:
+                cmap[pos[0]+k][pos[1]-k]=0
+    cmap[pos[0]][pos[1]]=5
+    return cmap
+
+
+N=int(input())
+raN=range(N)
+result=[]
+cnt=0
+f_chess_map=[[1 for _ in raN] for _ in raN]
+
+for i in raN:
+    for j in raN:
+        chess_map=copy.deepcopy(f_chess_map)
+        chess_map=change(chess_map,[i,j])
+        rec(0,chess_map)
+
+res=[]
+for num in range(len(result)):
+    Q_cnt=0
+    for i in raN:
+        Q_cnt+=result[num][i].count(5)
+    if Q_cnt==N:
+        res.append(result[num])
+idx_list=set()
+
+for i in range(len(res)):
+    for j in range(i+1,len(res)):
+        if res[i]==res[j]:
+            idx_list.add(i)
+
+for i in sorted(idx_list,reverse=True):
+    res.pop(i)     
+
+
+# for ab in raN:
+#     print(res[0][ab])
+# print()
+print(len(res))
